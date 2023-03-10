0f71a3b0559d535b418121d96f6b12b3e45f8305
exequielx <exequielx@gmail.com>
2023-03-10 16:29:16 -0300

    add

--- App.js
+++ App.js
@@ -1,20 +1,64 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, SafeAreaView, TextInput } from 'react-native';
 
 export default function App() {
+  const [link, setLink] = React.useState('');
+
+  React.useEffect(() => {
+    let value = localStorage.getItem('deeplink');
+    if (value) {
+      value = decodeURIComponent(value);
+      setLink(value);
+    }
+  }, [])
+
+  const changeLink = (r) => {
+    setLink(r);
+    localStorage.setItem('deeplink', encodeURIComponent(r))
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <div style={styles.content}>
+        <TextInput
+          style={styles.input}
+          placeholder='...'
+          multiline={true}
+          numberOfLines={10}
+          onChangeText={(r) => changeLink(r)}
+          value={link} />
+
+        <a style={styles.link} href={link}>{link}</a>
+      </div>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView >
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    fontSize: 18,
   },
+  content: {
+    height: '100%',
+    padding: '10px',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    backgroundColor: '#2a2828',
+  },
+  input: {
+    width: '100%',
+    borderColor: 'grey',
+    borderWidth: '1px',
+    color: 'white',
+    padding: 6,
+  },
+  link: {
+    paddingTop: '40px',
+    overflowWrap: 'break-word',
+    color: '#51D1F6',
+  }
 });
